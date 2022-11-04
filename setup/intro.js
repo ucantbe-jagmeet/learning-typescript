364a120861e4acac2ae2876d12b1a1340194c12f
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-11-04 12:17:26 +0530

    setting up tsc

---
+++ setup/intro.js
@@ -0,0 +1,6 @@
+var user = {
+    name: "hitesh",
+    age: 10
+};
+console.log("xyz");
+console.log(user.name);
---
+++ setup/intro.ts
@@ -0,0 +1,6 @@
+let user = {
+    name : "hitesh",
+    age : 10
+}
+console.log("xyz");
+console.log(user.name);
