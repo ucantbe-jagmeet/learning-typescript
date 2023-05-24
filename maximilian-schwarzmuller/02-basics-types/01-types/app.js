74c150fb20dbd1bed44b1d9113ca48c05efa8a73
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-24 22:13:55 +0530

    number, string and booleans

--- maximilian-schwarzmuller/02-basics-types/01-types/app.js
+++ maximilian-schwarzmuller/02-basics-types/01-types/app.js
@@ -1,10 +1,18 @@
-function add(n1, n2) {
-    if (typeof n1 !== "number" || typeof n2 !== "number") {
-        throw new Error("Incorrect Input");
+function add(n1, n2, showResult, phrase) {
+    // if(typeof n1 !== "number" || typeof n2 !== "number"){
+    //     throw new Error("Incorrect Input")
+    // }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
     }
-    return n1 + n2;
+    else {
+        return n1 + n2;
+    }
 }
-var number1 = "5";
+var number1 = 5;
 var number2 = 2.5;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result is: ";
+add(number1, number2, printResult, resultPhrase);
+// console.log(result);
