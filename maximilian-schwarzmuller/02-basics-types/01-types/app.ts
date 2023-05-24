8c00720d11f3a5570d54e7d502af1cf6451810bb
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-24 22:35:46 +0530

    type assignment and inference

--- maximilian-schwarzmuller/02-basics-types/01-types/app.ts
+++ maximilian-schwarzmuller/02-basics-types/01-types/app.ts
@@ -5,7 +5,7 @@ function add(n1:number, n2:number , showResult: boolean , phrase : string){
     // }
 
     const result = n1 + n2
-    
+
     if(showResult){
         console.log(phrase + result);
     } else {
@@ -13,10 +13,12 @@ function add(n1:number, n2:number , showResult: boolean , phrase : string){
     }
 }
 
-const number1 = 5
+let number1:number
+number1 = 5
 const number2 = 2.5
 const printResult = true
-const resultPhrase = "Result is: "
+let resultPhrase = "Result is: "
+// resultPhrase= 0
 
 add(number1, number2 , printResult , resultPhrase)
 // console.log(result);
