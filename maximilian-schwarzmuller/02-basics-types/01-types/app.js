9394bf6a912e8305544977f02a9ca689dbfa5a62
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-25 00:13:18 +0530

    Array types

--- maximilian-schwarzmuller/02-basics-types/01-types/app.js
+++ maximilian-schwarzmuller/02-basics-types/01-types/app.js
@@ -1,8 +1,18 @@
 // object types contains key types
 // {} is typescript notation of specialized object type
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
 var person = {
-    // const person = {
     name: "ucantbe",
-    age: 23
+    age: 23,
+    hobbies: ["sports", "cooking"],
 };
+var favorites;
+favorites = ["Traveling"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- maximilian-schwarzmuller/02-basics-types/01-types/app.ts
+++ maximilian-schwarzmuller/02-basics-types/01-types/app.ts
@@ -1,13 +1,23 @@
 // object types contains key types
 // {} is typescript notation of specialized object type
 
-const person:{
-    name: string;
-    age: number;
-} = {
-// const person = {
-    name: "ucantbe",
-    age: 23
-}
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+
+const person = {
+  name: "ucantbe",
+  age: 23,
+  hobbies: ["sports", "cooking"],
+};
+
+let favorites: string[];
+favorites = ["Traveling"];
 
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //   console.log(hobby.localeCompare(()));  // !!!ERROR!!!
+}
