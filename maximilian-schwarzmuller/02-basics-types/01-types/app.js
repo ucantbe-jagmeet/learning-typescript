bf6a8fdf45d867856c7a1e2674f593d0b97fc747
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-25 00:40:52 +0530

    enums

--- maximilian-schwarzmuller/02-basics-types/01-types/app.js
+++ maximilian-schwarzmuller/02-basics-types/01-types/app.js
@@ -4,10 +4,39 @@
 //   name: string;
 //   age: number;
 // } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   // const person = {
+//   name: "ucantbe",
+//   age: 23,
+//   hobbies: ["sports", "cooking"],
+//   role: [2, "author"],
+// };
+// tuples are the array of fixed length
+// person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [ 0, "admin", "user"]
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role["AUTHOR"] = "author";
+})(Role || (Role = {}));
+// after assigning the number 5 to the admin
+// other elements will get number 6,7
 var person = {
+    // const person = {
     name: "ucantbe",
     age: 23,
     hobbies: ["sports", "cooking"],
+    role: Role.AUTHOR,
 };
 var favorites;
 favorites = ["Traveling"];
@@ -15,4 +44,8 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    //   console.log(hobby.localeCompare(()));  // !!!ERROR!!!
+}
+if (person.role === Role.AUTHOR) {
+    console.log("is author");
 }
--- maximilian-schwarzmuller/02-basics-types/01-types/app.ts
+++ maximilian-schwarzmuller/02-basics-types/01-types/app.ts
@@ -5,11 +5,42 @@
 //   name: string;
 //   age: number;
 // } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   // const person = {
+//   name: "ucantbe",
+//   age: 23,
+//   hobbies: ["sports", "cooking"],
+//   role: [2, "author"],
+// };
+// tuples are the array of fixed length
+// person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [ 0, "admin", "user"]
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  "ADMIN" = 5,
+  "READ_ONLY" = 100,
+  "AUTHOR" = "author",
+}
+
+// after assigning the number 5 to the admin
+// other elements will get number 6,7
 
 const person = {
+  // const person = {
   name: "ucantbe",
   age: 23,
   hobbies: ["sports", "cooking"],
+  role: Role.AUTHOR,
 };
 
 let favorites: string[];
@@ -20,4 +51,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   //   console.log(hobby.localeCompare(()));  // !!!ERROR!!!
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("is author");
 }
