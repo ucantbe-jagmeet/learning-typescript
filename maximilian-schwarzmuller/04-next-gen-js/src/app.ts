9444f14b366b9b966f290605894181082076c6ba
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-25 20:39:20 +0530

    const and let

--- maximilian-schwarzmuller/04-next-gen-js/src/app.ts
+++ maximilian-schwarzmuller/04-next-gen-js/src/app.ts
@@ -1,1 +1,13 @@
-// Code goes here!
+const userName = "ucantbe";
+// userName = "Jagmeet";
+let age = 23;
+
+age = 22;
+
+var result;
+function add(a: number, b: number) {
+  result = a + b;
+  return result;
+}
+
+console.log(result);
