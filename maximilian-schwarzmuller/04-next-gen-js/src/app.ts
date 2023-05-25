a1e1e50fe0fd803c68063d231e1040e6eec860ff
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-25 21:04:28 +0530

    arror functions

--- maximilian-schwarzmuller/04-next-gen-js/src/app.ts
+++ maximilian-schwarzmuller/04-next-gen-js/src/app.ts
@@ -4,10 +4,22 @@ let age = 23;
 
 age = 22;
 
-var result;
-function add(a: number, b: number) {
-  result = a + b;
-  return result;
+// var result;
+// function add(a: number, b: number) {
+//   result = a + b;
+//   return result;
+// }
+
+// console.log(result);
+
+const add = (a: number, b: number) => a + b;
+
+const printOutput = (output: string | number) => console.log(output);
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
 }
 
-console.log(result);
+printOutput(add(66, 34));
