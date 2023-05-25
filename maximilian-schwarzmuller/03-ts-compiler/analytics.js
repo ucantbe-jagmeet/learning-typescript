ef319142657d09c16579bbd5b40ea13e6f19f728
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-25 20:14:33 +0530

    next gen js and ts

--- maximilian-schwarzmuller/03-ts-compiler/analytics.js
+++ maximilian-schwarzmuller/03-ts-compiler/analytics.js
@@ -1,4 +1,5 @@
 "use strict";
 // tsc --init
 // then type tsc , it will compile all the tsc files in that folder
+// then type tsc -w, it will recompile the tsc file whenever changes occur
 console.log("sending !!!");
