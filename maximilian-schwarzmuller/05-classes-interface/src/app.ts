acbdd3f159272e63ea3a049d57921216a6d1023a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-26 19:41:50 +0530

    Interface with classes

--- maximilian-schwarzmuller/05-classes-interface/src/app.ts
+++ maximilian-schwarzmuller/05-classes-interface/src/app.ts
@@ -1,20 +1,24 @@
 // interface -> it describe the structure of an object
 
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
-
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
 
-user1 = {
-  name: "jagmeet",
-  age: 23,
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
 
+let user1: Greetable;
+
+user1 = new Person("Jagmeet");
 user1.greet("Hi there - I am");
+console.log(user1);
