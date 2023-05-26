bf9185c6f980b61802cec02032c935c88c81040b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-26 17:14:24 +0530

    Override and protected modifiers

--- maximilian-schwarzmuller/05-classes-interface/src/app.ts
+++ maximilian-schwarzmuller/05-classes-interface/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -37,6 +37,13 @@ class AccountingDepartment extends Department {
     super(id, "Accounting");
   }
 
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -56,5 +63,8 @@ console.log(it);
 
 const acc = new AccountingDepartment("A1", ["Economics"]);
 acc.addReport("Business");
+acc.addEmployee("Max");
+acc.addEmployee("Manu");
 acc.printReports();
+// acc.printEmployeeInfo();
 console.log(acc);
