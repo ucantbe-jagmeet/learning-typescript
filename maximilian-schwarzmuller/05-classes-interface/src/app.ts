bda7f7bda4ac62bffb2318296ac93f3d4fcb7e16
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-26 15:58:00 +0530

    shorthand initialisation

--- maximilian-schwarzmuller/05-classes-interface/src/app.ts
+++ maximilian-schwarzmuller/05-classes-interface/src/app.ts
@@ -1,13 +1,15 @@
 class Department {
-  name: string;
+  // private id: string;
+  // private name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    this.id = id;
+    this.name = name;
   }
 
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(` Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -20,7 +22,7 @@ class Department {
   }
 }
 
-const accounting = new Department("Information Technology");
+const accounting = new Department("d1", "Information Technology");
 accounting.addEmployee("Jagmeet");
 accounting.addEmployee("Tony");
 
