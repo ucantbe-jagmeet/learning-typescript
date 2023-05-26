7ea777b875ca5c50b72fadc16e5b4c45b1157e87
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-26 15:46:42 +0530

    Public and private keyword

--- maximilian-schwarzmuller/05-classes-interface/src/app.ts
+++ maximilian-schwarzmuller/05-classes-interface/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,10 +9,25 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees);
+    console.log(this.employees.length);
+  }
 }
 
-const accounting = new Department("IT");
-accounting.describe();
+const accounting = new Department("Information Technology");
+accounting.addEmployee("Jagmeet");
+accounting.addEmployee("Tony");
 
-const accountingCopy = { name: "Dummy", describe: accounting.describe };
-accountingCopy.describe();
+// accounting.employees[2] = "Anna";
+
+accounting.describe();
+accounting.printEmployeeInfo();
+
+// const accountingCopy = { name: "Dummy", describe: accounting.describe };
+// accountingCopy.describe();
