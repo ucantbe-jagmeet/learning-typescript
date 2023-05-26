4e7ecaa9db78c3657fa784d7dff6cbf1739dabcb
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-26 15:34:55 +0530

    Constructor and this keyword

--- maximilian-schwarzmuller/05-classes-interface/src/app.ts
+++ maximilian-schwarzmuller/05-classes-interface/src/app.ts
@@ -4,7 +4,14 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
 }
 
 const accounting = new Department("IT");
-console.log(accounting);
+accounting.describe();
+
+const accountingCopy = { name: "Dummy", describe: accounting.describe };
+accountingCopy.describe();
