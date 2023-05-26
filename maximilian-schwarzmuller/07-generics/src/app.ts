cac48c1ddf9f7aa817d42aa46ad34db5dedb7ea3
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-27 00:25:40 +0530

    another generic function

--- maximilian-schwarzmuller/07-generics/src/app.ts
+++ maximilian-schwarzmuller/07-generics/src/app.ts
@@ -17,3 +17,23 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+
+  if (element.length === 1) {
+    descriptionText = "Got 1 element";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+
+  return [element, descriptionText];
+}
+// console.log(countAndDescribe(["hobbies"]));
+// console.log(countAndDescribe([]));
+// console.log(countAndDescribe(10));
+console.log(countAndDescribe("Hi there"));
