b239ea2beee51b47d23b1fe81c2c3a69fe8d1466
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-26 21:40:56 +0530

    type guards

--- maximilian-schwarzmuller/06-advanced-types/src/app.ts
+++ maximilian-schwarzmuller/06-advanced-types/src/app.ts
@@ -22,3 +22,58 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  // this if loop is called type guard
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name " + emp.name);
+
+  if ("privileges" in emp) {
+    console.log("Privileges " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date " + emp.startDate);
+  }
+}
+
+// printEmployeeInfo(e1);
+printEmployeeInfo({ name: "Manu", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving ......");
+  }
+}
+class Truck {
+  drive() {
+    console.log("Driving a Truck......");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading cargo... " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  // if ("loadCargo" in vehicle) {
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
