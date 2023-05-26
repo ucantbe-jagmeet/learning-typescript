c33d09922a8f90555cdfb1c37a6db777d3c0f195
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-26 21:12:27 +0530

    Intersection types

--- maximilian-schwarzmuller/06-advanced-types/src/app.ts
+++ maximilian-schwarzmuller/06-advanced-types/src/app.ts
@@ -1,1 +1,24 @@
-// code goes here
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+//interface ElevatedEmployee extends Employee, Admin {}
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Jagmeet",
+  privileges: ["Create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
