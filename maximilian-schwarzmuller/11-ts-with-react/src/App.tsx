909bdcb25c5ca1c913045177e19fb1cfbfd0f22c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-28 02:10:16 +0530

    setting states

--- maximilian-schwarzmuller/11-ts-with-react/src/App.tsx
+++ maximilian-schwarzmuller/11-ts-with-react/src/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
 
+import { Todo } from "./todo.model";
+
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "Finish the Course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addHandler = (text: string) => {
-    console.log(text);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
 
   return (
