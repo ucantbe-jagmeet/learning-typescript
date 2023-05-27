2e37af8cc5dc31ee6e1edc3edcc9d867fa61915f
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-28 03:44:45 +0530

    more CRUD functionality

--- maximilian-schwarzmuller/12-ts-with-nodejs/src/app.ts
+++ maximilian-schwarzmuller/12-ts-with-nodejs/src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
-
+import { json } from "body-parser";
 import todoRoutes from "./routes/todos";
 
 const app = express();
+
+app.use(json());
 
 app.use("/todos", todoRoutes);
 
@@ -11,4 +13,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("server is listening at port 3000");
+});
