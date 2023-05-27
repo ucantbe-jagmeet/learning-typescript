7096bcd6f6338f65e87558d811faaae223e7634c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-27 23:14:12 +0530

    axios to fetch data

--- maximilian-schwarzmuller/10-select-share-location/src/app.ts
+++ maximilian-schwarzmuller/10-select-share-location/src/app.ts
@@ -1,5 +1,14 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyCIaAc2c5M3VpbCH6PPq_guwy9lHouwXOs";
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(e: Event) {
   e.preventDefault();
@@ -7,6 +16,24 @@ function searchAddressHandler(e: Event) {
   const enteredAddress = addressInput.value;
 
   // send this to Google's API
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location");
+      }
+
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler);
