405435d5a62655323a351c65b8989babce54ec52
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-27 21:31:30 +0530

    class validators

--- maximilian-schwarzmuller/09-libs-with-ts/src/app.ts
+++ maximilian-schwarzmuller/09-libs-with-ts/src/app.ts
@@ -2,12 +2,24 @@ import "reflect-metadata";
 
 import { plainToClass } from "class-transformer";
 
+import { validate } from "class-validator";
+
 import { Product } from "./product.model";
 
 const products = [
   { title: "A Carpet", price: 29.99 },
   { title: "A BOok", price: 88.99 },
 ];
+
+const newProd = new Product("", -44.99);
+validate(newProd).then((err) => {
+  if (err.length > 0) {
+    console.log("VALIDATION ERRORS");
+    console.log(err);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
 
 // const p1 = new Product("A Book", 12.99);
 
