1c13a4f879c12349fd286dcaedabcf6653082405
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-28 03:00:07 +0530

    error handling middleware

--- maximilian-schwarzmuller/12-ts-with-nodejs/src/app.ts
+++ maximilian-schwarzmuller/12-ts-with-nodejs/src/app.ts
@@ -1,5 +1,14 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+
+import todoRoutes from "./routes/todos";
 
 const app = express();
+
+app.use("/todos", todoRoutes);
+
+// error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
