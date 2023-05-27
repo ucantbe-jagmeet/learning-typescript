c6d28227a4d7e0126bc4cdb861c1873873879e68
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-27 15:24:22 +0530

    generic utility types

--- maximilian-schwarzmuller/07-generics/src/app.ts
+++ maximilian-schwarzmuller/07-generics/src/app.ts
@@ -79,3 +79,24 @@ const numberStorage = new DataStorage<number>();
 // objStorage.addItem({ name: "manu" });
 // objStorage.removeItem(maxObj);
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names = ["Max", "Sports"];
+names.push("Manu");
