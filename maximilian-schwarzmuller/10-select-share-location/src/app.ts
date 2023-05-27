34bee708ee849bf3ab4ee4cf754ca6e53e8e452d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-27 22:41:47 +0530

    getting user input

--- maximilian-schwarzmuller/10-select-share-location/src/app.ts
+++ maximilian-schwarzmuller/10-select-share-location/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+function searchAddressHandler(e: Event) {
+  e.preventDefault();
+
+  const enteredAddress = addressInput.value;
+
+  // send this to Google's API
+}
+
+form.addEventListener("submit", searchAddressHandler);
