c5595f83197b0d195daa5038ee4b8a3c4f44b2ef
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-27 23:45:56 +0530

    rendering open layers

--- maximilian-schwarzmuller/10-select-share-location/src/app.ts
+++ maximilian-schwarzmuller/10-select-share-location/src/app.ts
@@ -1,48 +1,21 @@
-import axios from "axios";
+declare var ol: any;
 
-const form = document.querySelector("form")!;
-const addressInput = document.getElementById("address")! as HTMLInputElement;
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
 
-const GOOGLE_API_KEY = "AIzaSyCIaAc2c5M3VpbCH6PPq_guwy9lHouwXOs";
+  const coordinates = { lat: 40.41, lng: -73.99 }; // Can't fetch coordinates from Google API, use dummy ones
 
-// declare var google: any;
-
-type GoogleGeocodingResponse = {
-  results: { geometry: { location: { lat: number; lng: number } } }[];
-  status: "OK" | "ZERO_RESULTS";
-};
-
-function searchAddressHandler(e: Event) {
-  e.preventDefault();
-
-  const enteredAddress = addressInput.value;
-
-  // send this to Google's API
-
-  axios
-    .get<GoogleGeocodingResponse>(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-        enteredAddress
-      )}&key=${GOOGLE_API_KEY}`
-    )
-    .then((response) => {
-      if (response.data.status !== "OK") {
-        throw new Error("Could not fetch location");
-      }
-
-      const coordinates = response.data.results[0].geometry.location;
-
-      const map = new google.maps.Map(document.getElementById("map")!, {
-        center: coordinates,
-        zoom: 16,
-      });
-
-      new google.maps.Marker({ position: coordinates, map: map });
-    })
-    .catch((err) => {
-      alert(err.message);
-      console.log(err);
-    });
+  document.getElementById("map")!.innerHTML = ""; // clear <p> from <div id="map">
+  new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 16,
+    }),
+  });
 }
-
-form.addEventListener("submit", searchAddressHandler);
