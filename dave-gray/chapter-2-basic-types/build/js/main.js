a56aa69f8ce82649757bec4c33f34408852acb42
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-10 22:29:47 +0530

    chapter 2 completed

--- dave-gray/chapter-2-basic-types/build/js/main.js
+++ dave-gray/chapter-2-basic-types/build/js/main.js
@@ -1,8 +1,16 @@
 "use strict";
-let username = "jagmeet";
-console.log(username);
-let a = 12;
-let b = 6;
-let c = 2;
-console.log(a / b);
-console.log(c * b);
+let myName = 'Jagmeet';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+// isLoading = true
+isLoading = "true";
+album = 1984;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ dave-gray/chapter-2-basic-types/src/main.ts
@@ -0,0 +1,19 @@
+let myName: string = 'Jagmeet'
+let meaningOfLife: number 
+let isLoading: string | boolean
+let album: string | number
+
+myName = 'John'
+meaningOfLife = 42
+// isLoading = true
+isLoading = "true"
+album = 1984
+
+const sum = ( a:number, b:number) => {
+    return a+b
+}
+
+let postId: string | number
+let isActive: boolean | number
+
+let re : RegExp = /\w+/g
