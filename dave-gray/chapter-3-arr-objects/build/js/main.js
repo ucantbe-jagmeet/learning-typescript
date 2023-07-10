a25736ecfefad983d0941fbd525614ae53f51ec7
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-10 22:31:20 +0530

    chapter 3 setup

---
+++ dave-gray/chapter-3-arr-objects/build/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let myName = 'Jagmeet';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+// isLoading = true
+isLoading = "true";
+album = 1984;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
