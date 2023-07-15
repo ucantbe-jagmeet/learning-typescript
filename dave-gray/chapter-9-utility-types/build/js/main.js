0beaea3734904cc0daa6447ddd2d175ca8adeab0
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-15 23:00:44 +0530

    Readonly and Required

--- dave-gray/chapter-9-utility-types/build/js/main.js
+++ dave-gray/chapter-9-utility-types/build/js/main.js
@@ -10,3 +10,11 @@ const assign1 = {
 };
 const assignGraded = updateAssignment(assign1, { grade: 95 });
 console.log(assignGraded);
+// Required and Readonly
+const recordAssignment = (assign) => {
+    // send to database, etc
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+// assignVerified.grade = 99
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
--- dave-gray/chapter-9-utility-types/src/main.ts
+++ dave-gray/chapter-9-utility-types/src/main.ts
@@ -21,3 +21,15 @@ const assign1: Assignment = {
 
 const assignGraded : Assignment =  updateAssignment( assign1, {grade : 95})
 console.log( assignGraded);
+
+
+// Required and Readonly
+
+const recordAssignment = ( assign: Required<Assignment>):Assignment =>{
+    // send to database, etc
+    return assign
+}
+const assignVerified : Readonly<Assignment> =  { ...assignGraded , verified:true}
+
+// assignVerified.grade = 99
+recordAssignment({ ...assignGraded, verified: true})
